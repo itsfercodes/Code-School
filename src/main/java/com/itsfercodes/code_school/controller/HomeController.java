6f776a41ceb89992916da445cc184f42b0a951eb
Fernando-Lz <itsfercodes@outlook.com>
2023-06-08 11:43:54 -0600

    Add Thymeleaf template engine and first dynamic page

--- src/main/java/com/itsfercodes/code_school/controller/HomeController.java
+++ src/main/java/com/itsfercodes/code_school/controller/HomeController.java
@@ -1,13 +1,18 @@
 package com.itsfercodes.code_school.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
-  @GetMapping("/home")
-  public String displayHomePage() {
+  @GetMapping(value = { "", "/" })
+  // model is a container to hold the values for the template to be rendered
+  public String displayHomePage(Model model) {
+    // model works as a key value structure. Key should be the same as in the
+    // template
+    model.addAttribute("username", "Fernando");
     return "index.html";
   }
 
