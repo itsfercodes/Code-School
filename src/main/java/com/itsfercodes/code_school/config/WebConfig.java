19233b8cda14213a92c8453ce54cf3507911b167
Fernando-Lz <itsfercodes@outlook.com>
2023-06-09 12:42:58 -0600

    Add courses template and WebConfig class to map static pages

---
+++ src/main/java/com/itsfercodes/code_school/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.itsfercodes.code_school.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// Helps to map static pages that doesn't require any controller logic behind, saving us 
+// to create a controller to only display a static page
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+    registry.addViewController("/courses").setViewName("courses");
+  }
+}
