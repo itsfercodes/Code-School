addfb344556c89338fb797d45da7acd4ce5983a7
Fernando-Lz <itsfercodes@outlook.com>
2023-06-12 18:42:21 -0600

    Add links to index page and footer

--- src/main/java/com/itsfercodes/code_school/controller/ContactController.java
+++ src/main/java/com/itsfercodes/code_school/controller/ContactController.java
@@ -1,6 +1,5 @@
 package com.itsfercodes.code_school.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +21,7 @@ public class ContactController {
   }
 
   // Endpoints
-  @GetMapping("/contact")
+  @GetMapping("/contact-us")
   public String displayContactPage() {
     return "contact.html";
   }
