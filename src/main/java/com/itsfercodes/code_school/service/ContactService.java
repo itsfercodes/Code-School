2833e58aa592e5b9202e15a25e3abb1a453cbd06
Fernando-Lz <itsfercodes@outlook.com>
2023-06-13 12:04:27 -0600

    Add @Slf4j annotation to use logger

--- src/main/java/com/itsfercodes/code_school/service/ContactService.java
+++ src/main/java/com/itsfercodes/code_school/service/ContactService.java
@@ -4,13 +4,22 @@ import org.springframework.stereotype.Service;
 
 import com.itsfercodes.code_school.model.Contact;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class ContactService {
 
+  /**
+   * Save Contact details into DB
+   * 
+   * @param contact
+   * @return boolean
+   */
   public boolean saveMessageDetails(Contact contact) {
     boolean isSaved = true;
     // TODO: Save the data into a database
-    System.out.println(contact.toString());
+    log.info(contact.toString());
     return isSaved;
   }
 }
