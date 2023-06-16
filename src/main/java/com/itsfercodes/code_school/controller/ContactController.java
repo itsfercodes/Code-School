8f91de077e5c001c1b52ada8846bc6b646b3da32
Fernando-Lz <itsfercodes@outlook.com>
2023-06-16 12:34:20 -0600

    Add Contact Us form validation

--- src/main/java/com/itsfercodes/code_school/controller/ContactController.java
+++ src/main/java/com/itsfercodes/code_school/controller/ContactController.java
@@ -1,19 +1,26 @@
 package com.itsfercodes.code_school.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.itsfercodes.code_school.model.Contact;
 import com.itsfercodes.code_school.service.ContactService;
 
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class ContactController {
 
   private final ContactService contactService;
 
-  // Is optional as it is only on constructor, if there are more then it will
+  // Is optional as it is only one constructor, if there are more then it will
   // require it
   // @Autowired
   public ContactController(ContactService contactService) {
@@ -22,13 +29,23 @@ public class ContactController {
 
   // Endpoints
   @GetMapping("/contact-us")
-  public String displayContactPage() {
+  public String displayContactPage(Model model) {
+    model.addAttribute("contact", new Contact());
     return "contact.html";
   }
 
   @PostMapping(value = "/saveMessage")
-  public ModelAndView saveMessage(Contact contact) {
+  // The @ModelAttribute means that there is a model attribute named contact that
+  // is binded to the object contact. @Valid let spring know that there are
+  // validation inside the object
+  public String saveMessage(@Valid @ModelAttribute("contact") Contact contact, Errors errors) {
+
+    if (errors.hasErrors()) {
+      log.error("Contact form validation failed due to: " + errors.toString());
+      return "contact.html";
+    }
+
     contactService.saveMessageDetails(contact);
-    return new ModelAndView("redirect:/contact");
+    return "redirect:/contact-us";
   }
 }
