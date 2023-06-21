76b56b52b94754b1a1ef69a9b8153cb8dba1cc0b
Fernando-Lz <itsfercodes@outlook.com>
2023-06-21 12:08:36 -0600

    Add user and admin users (test purposes)

--- src/main/java/com/itsfercodes/code_school/config/ProjectSecurityConfig.java
+++ src/main/java/com/itsfercodes/code_school/config/ProjectSecurityConfig.java
@@ -3,12 +3,15 @@ package com.itsfercodes.code_school.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectSecurityConfig {
 
-  String[] staticResources = { "/assets/**", "/holidays/**", "/courses", "/contact-us", "/", "/about" };
+  String[] staticResources = { "/assets/**", "/holidays/**", "/courses", "/contact-us", "/", "", "/about" };
 
   @Bean
   SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
@@ -17,5 +20,24 @@ public class ProjectSecurityConfig {
         // Public
         .requestMatchers(staticResources).permitAll());
     return http.build();
+  }
+
+  // TODO: Create db to store user details
+  // Creates temporary user and admin credentials
+  @Bean
+  InMemoryUserDetailsManager userDetailsService() {
+    UserDetails user = User.withDefaultPasswordEncoder()
+        .username("user")
+        .password("12345")
+        .roles("USER")
+        .build();
+
+    UserDetails admin = User.withDefaultPasswordEncoder()
+        .username("admin")
+        .password("54321")
+        .roles("USER", "ADMIN")
+        .build();
+
+    return new InMemoryUserDetailsManager(user, admin);
   }
 }
